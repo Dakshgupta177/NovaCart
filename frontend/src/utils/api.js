e1fe7b6250ea4ac1309513256047b3723291ae0a
Dakshgupta177 <dakshgupta2k7@gmail.com>
2026-06-26 16:35:03 +0530

    added backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 const api = axios.create({
-  baseURL:  import.meta.env.VITE_BACKEND_URL || "http://localhost:8000",
+  baseURL:  "https://novacart-bnwg.onrender.com",
   withCredentials: true,
 });
 
